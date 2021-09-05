f0f7cf4b87338d68e0b0b0c3f089413579d31190
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-05 23:25:33 +0530

    Temparature Code Added

---
+++ Temperature/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Temperature
+{
+    class Program
+    {
+        public static double convertToFer(double celTemp)
+        {
+            double ferTemp = 0;
+            ferTemp = (celTemp * 9) / 5 + 32;
+            return ferTemp;
+        }
+        static void Main(string[] args)
+        {
+            double celTemp = 0;
+            double ferTemp = 0;
+          
+            Console.WriteLine("Enter the Value of Temparature in Celcius");
+            celTemp = double.Parse(Console.ReadLine());
+
+            ferTemp = convertToFer(celTemp);
+            Console.WriteLine("Fahrenheit temparature is:" + ferTemp);  
+        }
+    }
+}
