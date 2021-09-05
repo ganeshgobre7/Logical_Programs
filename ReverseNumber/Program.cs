cfe03c56242a7517b47225c34dd4199385e13a2d
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-05 23:22:33 +0530

    Reverse Number Code Added

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, revNo=0, rem;
+            Console.WriteLine("Enter the Number You Want to Reverse");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num > 0)
+            {
+                rem = num % 10;
+                revNo = revNo * 10 + rem;
+                num = num / 10;
+
+            }
+            Console.WriteLine("Reverse Number is:" + revNo);
+        }
+    }
+}
