1b89bb1c4122d55e628dc94d4d43eda5afaee69d
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-05 23:19:21 +0530

    Perfect Number Code Added

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0;
+            Console.WriteLine("Enter the Number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            if (sum == num)
+            {
+                Console.WriteLine("Entered Number is Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine("Entered Number is Not Perfect Number");
+            }
+        }
+    }
+}
