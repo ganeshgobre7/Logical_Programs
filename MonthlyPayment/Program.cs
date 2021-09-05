602245c4b84a0e90a9e125b66741567709a95a29
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-05 23:26:55 +0530

    Monthly Payment Code Added

---
+++ MonthlyPayment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MonthlyPayment
+{
+    class Program
+    {
+        public static void monthlyPayment(double p,double y,double r)
+        {
+            double n = 12 * y;
+            r = r / (12 * 100);
+            double payment = (p * r) / (1 - Math.Abs(Math.Pow((1 + r), (-n))));
+            Console.WriteLine("Total Monthly Payment is:"+ payment);
+        }
+        static void Main(string[] args)
+        {
+            double p, y, r;
+
+            Console.WriteLine("Enter the Loan Amount");
+            double amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Year");
+            double year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Monthly Intrest");
+            double intrest = Convert.ToInt32(Console.ReadLine());
+
+            monthlyPayment(amount,year,intrest);
+        }
+    }
+}
