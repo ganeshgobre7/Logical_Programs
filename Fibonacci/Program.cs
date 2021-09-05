7002d77fde2daa7b1e5fbceef00a0c561c5336f5
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-05 23:16:37 +0530

    Finbonacci Code Added

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0;
+            int q = 1;
+            int temp = 0;
+
+            Console.WriteLine("Enter How Many Terms You Want");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci Series of Entered terms is");
+
+            Console.WriteLine(p);
+            Console.WriteLine(q);
+
+            for (int i = 2; i < input; i++)
+            {
+                temp = p + q;
+                Console.WriteLine(temp);
+                p = q;
+                q = temp;
+
+            }
+        }
+    }
+}
