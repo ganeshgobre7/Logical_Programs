1ad388dc580a7a346f0b4641c7afe3a7b79edaaf
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-05 23:20:51 +0530

    Prime Number Code Added

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;           
+            Boolean flag = true;
+            Console.WriteLine("Enter the Number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=2;i<number;i++)
+            {
+                if (number % i == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+            {
+                Console.WriteLine("Number is Prime");
+            }
+            else
+            {
+                Console.WriteLine("Number is Not Prime");
+            }
+        }
+    }
+}
