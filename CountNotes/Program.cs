7fb595f8ec75afe4cd89628b9ede19ce997c4353
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-06 11:22:25 +0530

    Count Notes code Addded

---
+++ CountNotes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount to get the minimum values of the notes");
+            int amt = Convert.ToInt32(Console.ReadLine());
+
+            int [] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+
+            for (int i = 0;  i < notes.Length;i++)
+            {
+                if (amt >= notes[i]) 
+                {
+                    Console.WriteLine(notes[i] + "=" + amt / notes[i] + " notes");
+                    amt = amt % notes[i]; 
+                }
+            }
+        }
+    }
+}
